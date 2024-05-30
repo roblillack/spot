0db2ef725ab359d63cefd0a20e843f2ad003e278
Robert Lillack <rob@lillack.net>
2024-05-30 18:10:43 +0200

    cocoa: Don't log all window events.

--- ui/internal/cocoa/windowdelegate.m
+++ ui/internal/cocoa/windowdelegate.m
@@ -1,56 +1,52 @@
 #import "windowdelegate.h"
 #include "_cgo_export.h"
 
-
-const int DID_RESIZE_EVENT           = 0;
-const int DID_MOVE_EVENT             = 1;
-const int DID_MINIATURIZE_EVENT      = 2;
-const int DID_DEMINIATURIZE_EVENT    = 3;
-
+const int DID_RESIZE_EVENT = 0;
+const int DID_MOVE_EVENT = 1;
+const int DID_MINIATURIZE_EVENT = 2;
+const int DID_DEMINIATURIZE_EVENT = 3;
 
 @implementation WindowDelegate
 
-- (void)dealloc
-{
-    [super dealloc];
+- (void)dealloc {
+  [super dealloc];
 }
 
-- (void)windowDidResize:(NSNotification *)notification
-{
-    NSWindow *movedWindow = notification.object;
-    triggerEvent([self goWindowID], movedWindow, @"windowDidResize", DID_RESIZE_EVENT);
+- (void)windowDidResize:(NSNotification *)notification {
+  NSWindow *movedWindow = notification.object;
+  triggerEvent([self goWindowID], movedWindow, @"windowDidResize",
+               DID_RESIZE_EVENT);
 }
 
-- (void)windowDidMove:(NSNotification *)notification
-{
-    NSWindow *movedWindow = notification.object;
-    triggerEvent([self goWindowID], movedWindow, @"windowDidMove", DID_MOVE_EVENT);
+- (void)windowDidMove:(NSNotification *)notification {
+  NSWindow *movedWindow = notification.object;
+  triggerEvent([self goWindowID], movedWindow, @"windowDidMove",
+               DID_MOVE_EVENT);
 }
 
-- (void)windowDidMiniaturize:(NSNotification *)notification
-{
-    NSWindow *movedWindow = notification.object;
-    triggerEvent([self goWindowID], movedWindow, @"windowDidMiniaturize", DID_MINIATURIZE_EVENT);
+- (void)windowDidMiniaturize:(NSNotification *)notification {
+  NSWindow *movedWindow = notification.object;
+  triggerEvent([self goWindowID], movedWindow, @"windowDidMiniaturize",
+               DID_MINIATURIZE_EVENT);
 }
 
-- (void)windowDidDeminiaturize:(NSNotification *)notification
-{
-    NSWindow *movedWindow = notification.object;
-    triggerEvent([self goWindowID], movedWindow, @"windowDidDeminiaturize", DID_DEMINIATURIZE_EVENT);
+- (void)windowDidDeminiaturize:(NSNotification *)notification {
+  NSWindow *movedWindow = notification.object;
+  triggerEvent([self goWindowID], movedWindow, @"windowDidDeminiaturize",
+               DID_DEMINIATURIZE_EVENT);
 }
 
 @end
 
-void triggerEvent(int goWindowID, NSWindow *movedWindow, NSString *eventTitle, const int eventId) 
-{
-    if ([movedWindow isKeyWindow])
-    {
-        NSRect rect = movedWindow.frame;
-        int x = (int)(rect.origin.x);
-        int y = (int)(rect.origin.y);
-        int w = (int)(rect.size.width);
-        int h = (int)(rect.size.height);
-        NSLog(@"%@ %@", eventTitle, movedWindow);
-        onWindowEvent(goWindowID, eventId, x, y, w, h);
-    }
+void triggerEvent(int goWindowID, NSWindow *movedWindow, NSString *eventTitle,
+                  const int eventId) {
+  if ([movedWindow isKeyWindow]) {
+    NSRect rect = movedWindow.frame;
+    int x = (int)(rect.origin.x);
+    int y = (int)(rect.origin.y);
+    int w = (int)(rect.size.width);
+    int h = (int)(rect.size.height);
+    // NSLog(@"%@ %@", eventTitle, movedWindow);
+    onWindowEvent(goWindowID, eventId, x, y, w, h);
+  }
 }
