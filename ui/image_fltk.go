c3aafeeacd6d12b055a1baa8b1eaa95c5375d6a2
Robert Lillack <rob@lillack.net>
2024-12-19 09:17:07 +0100

    cocoa/fltk: Fix image memory leaks in native code. #30 (#31)
    * cocoa: Add NSImages and NSBitmapImageReps into autorelease pool. #30
    
    * fltk: Destroy old RgbImage when setting a new one. #30

--- ui/image_fltk.go
+++ ui/image_fltk.go
@@ -9,7 +9,14 @@ import (
 	"github.com/roblillack/spot"
 )
 
-type nativeTypeImage = *goFltk.Box
+// There's currently no way to get the current RgbImage out of the
+// box, so we have to keep a reference to it.
+type fltkImageWrapper struct {
+	box *goFltk.Box
+	img *goFltk.RgbImage
+}
+
+type nativeTypeImage = *fltkImageWrapper
 
 func (c *Image) Update(nextControl spot.Control) bool {
 	next, ok := nextControl.(*Image)
@@ -37,12 +44,15 @@ func (c *Image) Mount(parent spot.Control) any {
 		return nil
 	}
 
-	c.ref = goFltk.NewBox(goFltk.DOWN_BOX, c.X, c.Y, c.Width, c.Height)
-	c.ref.SetEventHandler(c.handleEvent)
+	box := goFltk.NewBox(goFltk.DOWN_BOX, c.X, c.Y, c.Width, c.Height)
+	box.SetEventHandler(c.handleEvent)
+	c.ref = &fltkImageWrapper{
+		box: box,
+	}
 	c.draw()
 
 	if window, ok := parent.(*Window); ok && window != nil && window.ref != nil {
-		window.ref.Add(c.ref)
+		window.ref.Add(box)
 	}
 
 	return c.ref
@@ -53,7 +63,14 @@ func (b *Image) Unmount() {
 		return
 	}
 
-	b.ref.Destroy()
+	if b.ref.img != nil {
+		b.ref.img.Destroy()
+	}
+
+	if b.ref.box != nil {
+		b.ref.box.Destroy()
+	}
+
 	b.ref = nil
 }
 
@@ -84,6 +101,11 @@ func (c *Image) draw() {
 		log.Println(err)
 		return
 	}
-	c.ref.SetImage(fimg)
-	c.ref.Redraw()
+	oldImg := c.ref.img
+	c.ref.img = fimg
+	c.ref.box.SetImage(fimg)
+	c.ref.box.Redraw()
+	if oldImg != nil {
+		oldImg.Destroy()
+	}
 }
